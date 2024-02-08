55379bdad22b25b86d257f22aad9f1c4a20c60f2
Joseph Wilde <jwilde19@gmail.com>
2024-02-08 09:07:36 -0500

    Create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+// React imports
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Component imports
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <>
-      <h1 className='text-9xl'>Hello World</h1>
+      <Navbar />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <h1 className='text-9xl text-center pt-20 bg-slate-400 h-[100dvh]'>
+              Hello World
+            </h1>
+          }
+        />
+        <Route path='/stories' element={<h1>stories</h1>} />
+        <Route path='/features' element={<h1>features</h1>} />
+        <Route path='/pricing' element={<h1>pricing</h1>} />
+      </Routes>
     </>
   )
 }
