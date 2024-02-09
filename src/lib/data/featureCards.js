85c94181df08f8bfcf5ea31665f10208b8c515e9
Joseph Wilde <jwilde19@gmail.com>
2024-02-09 18:24:08 -0500

    Create files

--- src/lib/data/featureCards.js
+++ src/lib/data/featureCards.js
@@ -1,15 +1,18 @@
 const featureCards = [
   {
+    key: '1',
     src: 'src/assets/shared/responsive-icon.svg',
     heading: '100% Responsive',
     body: 'No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen.',
   },
   {
+    key: '2',
     src: 'src/assets/shared/infinity-icon.svg',
     heading: 'No Photo Upload Limit',
     body: 'Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go.',
   },
   {
+    key: '3',
     src: 'src/assets/shared/embed-icon.svg',
     heading: 'Available to Embed',
     body: 'Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more. ',
--- src/lib/data/storyCards.js
+++ src/lib/data/storyCards.js
@@ -1,20 +1,24 @@
 const storyCards = [
   {
+    key: '1',
     srcMobile: 'src/assets/stories/mobile/mountains.jpg',
     heading: 'The Mountains',
     author: 'John Appleseed',
   },
   {
+    key: '2',
     srcMobile: 'src/assets/stories/mobile/cityscapes.jpg',
     heading: 'Sunset Cityscapes',
     author: 'Benjamin Cruz',
   },
   {
+    key: '3',
     srcMobile: 'src/assets/stories/mobile/18-days-voyage.jpg',
     heading: '18 Days Voyage',
     author: 'Alexei Borodin',
   },
   {
+    key: '4',
     srcMobile: 'src/assets/stories/mobile/architecturals.jpg',
     heading: 'Architecturals',
     author: 'Samantha Brooke',
