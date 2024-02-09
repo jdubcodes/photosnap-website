9d57baf4729dd232a2ca416b28d71360598fe77f
Joseph Wilde <jwilde19@gmail.com>
2024-02-09 18:23:36 -0500

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Component imports
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Home from './components/Home'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/features' element={<h1>features</h1>} />
         <Route path='/pricing' element={<h1>pricing</h1>} />
       </Routes>
+      <Footer />
     </>
   )
 }
