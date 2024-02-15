7d35b72950a6e85fef38bf91d3299685863a2237
Joseph Wilde <jwilde19@gmail.com>
2024-02-15 11:48:49 -0500

    Create data files

---
+++ src/lib/data/menu.js
@@ -0,0 +1,19 @@
+const menu = [
+  {
+    key: '1',
+    name: 'Stories',
+    src: '/stories',
+  },
+  {
+    key: '2',
+    name: 'Features',
+    src: '/features',
+  },
+  {
+    key: '3',
+    name: 'Pricing',
+    src: '/pricing',
+  },
+]
+
+export default menu
---
+++ src/lib/data/socials.js
@@ -0,0 +1,29 @@
+const socials = [
+  {
+    key: '1',
+    name: 'facebook',
+    src: 'src/assets/shared/desktop/facebook.svg',
+  },
+  {
+    key: '2',
+    name: 'youtube',
+    src: 'src/assets/shared/desktop/youtube.svg',
+  },
+  {
+    key: '3',
+    name: 'twitter',
+    src: 'src/assets/shared/desktop/twitter.svg',
+  },
+  {
+    key: '4',
+    name: 'pinterest',
+    src: 'src/assets/shared/desktop/pinterest.svg',
+  },
+  {
+    key: '5',
+    name: 'instagram',
+    src: 'src/assets/shared/desktop/instagram.svg',
+  },
+]
+
+export default socials
