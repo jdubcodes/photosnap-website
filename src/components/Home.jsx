fbd2f8dc98e9c85e5a5c7009141c29f86b0d8c8a
Joseph Wilde <jwilde19@gmail.com>
2024-02-15 20:07:13 -0500

    Finish hero section for all screen sizes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,28 +3,34 @@ import benefitCards from '../lib/data/benefitCards'
 import storyCards from '../lib/data/storyCards'
 import featureCards from '../lib/data/featureCards'
 // Imported assets
-import hero from '../assets/home/mobile/create-and-share.jpg'
+import heroMobile from '../assets/home/mobile/create-and-share.jpg'
+import heroTablet from '../assets/home/tablet/create-and-share.jpg'
+import heroDesktop from '../assets/home/desktop/create-and-share.jpg'
 import arrowLight from '../assets/shared/arrowLight.svg'
 import arrowDark from '../assets/shared/arrowDark.svg'
+// Imported components
+import LightInviteBtn from './helpers/LightInviteBtn'
 
 export default function Home() {
   return (
     <main>
-      <section>
-        <img src={hero} alt='hero image' className='w-full h-hero-img' />
-        <div className='px-9 py-19 flex flex-col gap-6 bg-black text-white'>
-          <h1 className='text-heading uppercase text-balance'>
+      <section className='bg-black md:grid md:grid-cols-home-tablet md:place-items-center lg:grid-cols-home-desktop'>
+        <img
+          src={heroDesktop}
+          srcSet={`${heroMobile} 767w, ${heroTablet} 1439w, ${heroDesktop} 1440w`}
+          alt='hero image'
+          className='w-full h-hero-mobile object-cover object-[50%_35%] md:h-hero-tablet md:col-start-2'
+        />
+        <div className='px-9 py-19 flex flex-col gap-6 text-white md:row-start-1 md:max-w-[378px] md:px-0'>
+          <h1 className='text-heading-mobile uppercase text-balance md:text-heading-tablet md:text-wrap'>
             Create and share your photo stories.
           </h1>
-          <p className='text-body text-white/60'>
+          <p className='mb-2 max-w-[400px] text-body text-white/60'>
             Photosnap is a platform for photographers and visual storytellers.
             We make it easy to share photos, tell stories and connect with
             others.
           </p>
-          <button className='mt-2 flex items-center gap-[1.125rem] text-link font-bold uppercase text-left'>
-            Get an invite
-            <img src={arrowLight} alt='arrow' />
-          </button>
+          <LightInviteBtn />
         </div>
       </section>
       <section>
