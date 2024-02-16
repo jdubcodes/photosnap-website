3b11baaf279153295cb5588fd6c5f4e77915da6d
Joseph Wilde <jwilde19@gmail.com>
2024-02-16 11:23:03 -0500

    Add tablet and desktop sources

--- src/lib/data/benefitCards.js
+++ src/lib/data/benefitCards.js
@@ -1,13 +1,17 @@
 const benefitCards = [
   {
     key: '1',
-    srcMobile: 'beautiful-stories.jpg',
+    srcMobile: 'mobile/beautiful-stories.jpg',
+    srcTablet: 'tablet/beautiful-stories.jpg',
+    srcDesktop: 'desktop/beautiful-stories.jpg',
     heading: 'Beautiful stories every time',
     body: 'We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.',
   },
   {
     key: '2',
-    srcMobile: 'designed-for-everyone.jpg',
+    srcMobile: 'mobile/designed-for-everyone.jpg',
+    srcTablet: 'tablet/designed-for-everyone.jpg',
+    srcDesktop: 'desktop/designed-for-everyone.jpg',
     heading: 'Designed for everyone',
     body: 'Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it. ',
   },
