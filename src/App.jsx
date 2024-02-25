ac18afd60898aa58cb7034521c0751816546a8b9
Joseph Wilde <jwilde19@gmail.com>
2024-02-24 20:29:27 -0500

    Add pricing component to route

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import Home from './components/Home'
 import Stories from './components/Stories'
 import Features from './components/Features'
+import Pricing from './components/Pricing'
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/stories' element={<Stories />} />
         <Route path='/features' element={<Features />} />
-        <Route path='/pricing' element={<h1>pricing</h1>} />
+        <Route path='/pricing' element={<Pricing />} />
       </Routes>
       <Footer />
     </>
