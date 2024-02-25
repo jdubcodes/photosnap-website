d84ffa63e7f49623c07ec1b39de518b7756e0b1a
Joseph Wilde <jwilde19@gmail.com>
2024-02-24 20:30:26 -0500

    Complete first section for mobile screens

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,31 @@
+// Imported custom compoenents
+import Cta from './utilities/Cta'
+// Imported assets
+import pricingHero from '../assets/pricing/desktop/hero.jpg'
+
+export default function Pricing() {
+  return (
+    <>
+      <section className='bg-black text-white md:h-[30.625rem] md:grid md:grid-cols-home-tablet md:grid-rows-1 md:items-center lg:h-hero-tablet lg:grid-cols-home-tablet-reverse'>
+        <img
+          src={pricingHero}
+          alt='hero image'
+          className='w-full h-[294px] object-cover object-top md:h-full md:col-start-2 md:object-[30%]'
+        />
+        <div className='px-7 py-[4.5rem] flex flex-col gap-4 md:px-10 md:col-start-1 md:row-start-1 lg:mx-auto'>
+          <h1 className='text-heading-mobile uppercase md:text-heading-tablet'>
+            Pricing
+          </h1>
+          <p className='max-w-[24.063rem] text-body text-white/60'>
+            Create a your stories, Photosnap is a platform for photographers and
+            visual storytellers. It’s the simple way to create and share your
+            photos.
+          </p>
+        </div>
+      </section>
+      <section></section>
+      <section></section>
+      <Cta />
+    </>
+  )
+}
